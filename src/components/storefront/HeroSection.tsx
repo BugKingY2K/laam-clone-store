a8db3ed343f4e9f77097fd5faf5dce5ea4695ebf
BugKingY2K <waqsak@hotmail.com>
2026-06-18 17:50:03 +0500

    Create HeroSection.tsx

---
+++ src/components/storefront/HeroSection.tsx
@@ -0,0 +1,38 @@
+export default function HeroSection() {
+
+  return (
+
+    <section className="bg-gray-100">
+
+      <div className="container-custom py-24">
+
+        <h1 className="text-6xl font-bold">
+
+          Fashion For Everyone
+
+        </h1>
+
+        <p className="mt-4 text-xl">
+
+          Discover premium Products
+          inspired by Products of Pakistan
+
+        </p>
+
+        <button
+          className="
+          mt-8
+          px-8
+          py-4
+          bg-black
+          text-white
+          rounded-lg"
+        >
+          Shop Now
+        </button>
+
+      </div>
+
+    </section>
+  );
+}
