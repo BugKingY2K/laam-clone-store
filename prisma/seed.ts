a5e4b79b6692e3f2f879a9df18e294bd423439f0
BugKingY2K <waqsak@hotmail.com>
2026-06-18 17:32:19 +0500

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
 
+const prisma = new PrismaClient();
+
+async function main() {
+
+  const password =
+    await bcrypt.hash(
+      "Admin123!",
+      12
+    );
+
+  await prisma.user.upsert({
+
+    where: {
+      email: "admin@yourstore.com"
+    },
+
+    update: {},
+
+    create: {
+
+      firstName: "Store",
+
+      lastName: "Admin",
+
+      email: "admin@yourstore.com",
+
+      passwordHash: password,
+
+      role: "ADMIN"
+    }
+  });
+
+  console.log("Admin Created");
+}
+
+main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
