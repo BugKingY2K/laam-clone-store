d43bd0ae1c55461a95fe86781865233322f3b47d
BugKingY2K <waqsak@hotmail.com>
2026-06-18 22:16:34 +0500

    Create AddToCartButton.tsx

---
+++ src/components/storefront/AddToCartButton.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useCartStore }
+from "@/store/cart-store";
+
+type Props = {
+
+  productId: string;
+
+  name: string;
+
+  price: number;
+};
+
+export default function AddToCartButton({
+
+  productId,
+
+  name,
+
+  price
+
+}: Props) {
+
+  const addItem =
+    useCartStore(
+      state => state.addItem
+    );
+
+  return (
+
+    <button
+
+      onClick={() =>
+        addItem({
+
+          productId,
+
+          name,
+
+          price,
+
+          quantity: 1
+        })
+      }
+
+      className="
+      bg-black
+      text-white
+      px-4
+      py-2
+      rounded"
+    >
+
+      Add To Cart
+
+    </button>
+  );
+}
