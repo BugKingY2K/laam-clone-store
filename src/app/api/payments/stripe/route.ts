49990241ae2f3f28532928371dcb83b773730e9a
BugKingY2K <waqsak@hotmail.com>
2026-06-18 22:23:03 +0500

    Create route.ts

---
+++ src/app/api/payments/stripe/route.ts
@@ -0,0 +1,29 @@
+import { stripe }
+from "@/lib/stripe";
+
+export async function POST(
+  request: Request
+) {
+
+  const body =
+    await request.json();
+
+  const session =
+    await stripe.checkout.sessions.create({
+
+      mode: "payment",
+
+      line_items:
+        body.items,
+
+      success_url:
+        `${process.env.NEXTAUTH_URL}/success`,
+
+      cancel_url:
+        `${process.env.NEXTAUTH_URL}/cart`
+    });
+
+  return Response.json({
+    url: session.url
+  });
+}
