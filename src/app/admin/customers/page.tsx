701a48df14f0770ace1b38b1b654477fa9b141d9
BugKingY2K <waqsak@hotmail.com>
2026-06-18 21:19:09 +0500

    Create page.tsx

---
+++ src/app/admin/customers/page.tsx
@@ -0,0 +1,19 @@
+export default function CustomersPage() {
+
+  return (
+
+    <div>
+
+      <h1
+        className="
+        text-3xl
+        font-bold"
+      >
+
+        Customers
+
+      </h1>
+
+    </div>
+  );
+}
