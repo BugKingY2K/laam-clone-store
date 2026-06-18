0bc02807f110c36dfb32d64d23ec41a33c87bad8
BugKingY2K <waqsak@hotmail.com>
2026-06-18 22:06:26 +0500

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,17 @@
 import AdminSidebar from "@/components/admin/AdminSidebar";
 import AdminHeader from "@/components/admin/AdminHeader";
 
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+
+const session =
+  await auth();
+
+if (!session) {
+
+  redirect("/login");
+}
+
 export default function AdminLayout({
   children,
 }: {
