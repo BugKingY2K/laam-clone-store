5ff45d5f8faf26e9f515953a8f214deb21070bd9
BugKingY2K <waqsak@hotmail.com>
2026-06-18 18:18:56 +0500

    Create page.tsx

---
+++ src/app/account/order/page.tsx
@@ -0,0 +1,15 @@
+export default function OrdersPage() {
+
+  return (
+
+    <div className="container-custom py-10">
+
+      <h1 className="text-3xl font-bold">
+
+        My Orders
+
+      </h1>
+
+    </div>
+  );
+}
