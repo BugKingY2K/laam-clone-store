2b086868df673dc90d28baa00ce97cfe0c00bfbf
BugKingY2K <waqsak@hotmail.com>
2026-06-18 18:20:42 +0500

    Update page.tsx

--- src/app/account/wishlist/page.tsx
+++ src/app/account/wishlist/page.tsx
@@ -1,1 +1,15 @@
+export default function WishlistPage() {
 
+  return (
+
+    <div className="container-custom py-10">
+
+      <h1 className="text-3xl font-bold">
+
+        Wishlist
+
+      </h1>
+
+    </div>
+  );
+}
