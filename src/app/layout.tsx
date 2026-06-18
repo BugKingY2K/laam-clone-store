b191ea32bfb81697cfaf25306bbc729b417f8c4c
BugKingY2K <waqsak@hotmail.com>
2026-06-18 17:41:59 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,31 @@
+import "./globals.css";
+import Header from "@/components/storefront/Header";
+import Footer from "@/components/storefront/Footer";
 
+export const metadata = {
+  title: "LAAM Clone Store",
+  description: "Premium Fashion Marketplace"
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+
+  return (
+    <html lang="en">
+      <body>
+
+        <Header />
+
+        <main>
+          {children}
+        </main>
+
+        <Footer />
+
+      </body>
+    </html>
+  );
+}
