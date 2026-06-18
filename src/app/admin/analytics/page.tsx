50bc45adb2fdce1c6273c9cfb60515db59d4e1ce
BugKingY2K <waqsak@hotmail.com>
2026-06-18 21:20:55 +0500

    Create page.tsx

---
+++ src/app/admin/analytics/page.tsx
@@ -0,0 +1,19 @@
+export default function AnalyticsPage() {
+
+  return (
+
+    <div>
+
+      <h1
+        className="
+        text-3xl
+        font-bold"
+      >
+
+        Analytics
+
+      </h1>
+
+    </div>
+  );
+}
