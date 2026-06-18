6b9bbe9ec1f1547103e0aeee5223e058feb5b96a
BugKingY2K <waqsak@hotmail.com>
2026-06-18 18:17:04 +0500

    Create route.ts

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcryptjs";
+
+import { prisma }
+from "@/lib/prisma";
+
+export async function POST(
+  request: Request
+) {
+
+  const body =
+    await request.json();
+
+  const passwordHash =
+    await bcrypt.hash(
+      body.password,
+      12
+    );
+
+  const user =
+    await prisma.user.create({
+
+      data: {
+
+        firstName:
+          body.firstName,
+
+        lastName:
+          body.lastName,
+
+        email:
+          body.email,
+
+        passwordHash
+      }
+    });
+
+  return Response.json(user);
+}
