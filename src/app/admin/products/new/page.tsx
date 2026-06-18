628fdde006022ecaf12f7c7f2f7033ed0e998030
BugKingY2K <waqsak@hotmail.com>
2026-06-18 21:13:47 +0500

    Create page.tsx

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,26 @@
+import ProductForm
+from "@/components/admin/ProductForm";
+
+export default function
+NewProductPage() {
+
+  return (
+
+    <div>
+
+      <h1
+        className="
+        text-3xl
+        font-bold
+        mb-6"
+      >
+
+        Create Product
+
+      </h1>
+
+      <ProductForm />
+
+    </div>
+  );
+}
