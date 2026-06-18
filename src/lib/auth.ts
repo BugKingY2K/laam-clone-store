7f9e8a7c69f53a5dbb992a306f1b6bd980696a5b
BugKingY2K <waqsak@hotmail.com>
2026-06-18 17:30:34 +0500

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,1 +1,54 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "./prisma";
+import bcrypt from "bcryptjs";
 
+export const { handlers, auth, signIn, signOut } =
+  NextAuth({
+    providers: [
+      Credentials({
+        credentials: {
+          email: {},
+          password: {}
+        },
+
+        async authorize(credentials) {
+
+          if (!credentials?.email) {
+            return null;
+          }
+
+          const user =
+            await prisma.user.findUnique({
+              where: {
+                email:
+                  credentials.email as string
+              }
+            });
+
+          if (!user) {
+            return null;
+          }
+
+          const valid =
+            await bcrypt.compare(
+              credentials.password as string,
+              user.passwordHash
+            );
+
+          if (!valid) {
+            return null;
+          }
+
+          return {
+            id: user.id,
+            email: user.email,
+            name:
+              user.firstName +
+              " " +
+              user.lastName
+          };
+        }
+      })
+    ]
+  });
