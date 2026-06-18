0d95d1cd7b5c39ef9c971cf6a02786c969a3b873
BugKingY2K <waqsak@hotmail.com>
2026-06-18 17:48:24 +0500

    Create Footer.tsx

---
+++ src/components/storefront/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+
+  return (
+
+    <footer className="border-t mt-16">
+
+      <div className="container-custom py-12">
+
+        <p className="text-center">
+
+          © 2026 Products of Pakistan Store
+
+        </p>
+
+      </div>
+
+    </footer>
+  );
+}
