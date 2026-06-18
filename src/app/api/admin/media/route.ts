5ec759fb6f3db747d5fe9e9f55ed73b5ed89416f
BugKingY2K <waqsak@hotmail.com>
2026-06-18 22:03:02 +0500

    Create route.ts

---
+++ src/app/api/admin/media/route.ts
@@ -0,0 +1,20 @@
+import cloudinary
+from "@/lib/cloudinary";
+
+export async function POST(
+  request: Request
+) {
+
+  const body =
+    await request.json();
+
+  const result =
+    await cloudinary.uploader.upload(
+      body.image
+    );
+
+  return Response.json({
+    url:
+      result.secure_url
+  });
+}
