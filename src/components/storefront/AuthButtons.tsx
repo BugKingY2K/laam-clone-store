f79017baecb429606fef8d7eaeda06ddc5f415c5
BugKingY2K <waqsak@hotmail.com>
2026-06-18 18:25:40 +0500

    Create AuthButtons.tsx

---
+++ src/components/storefront/AuthButtons.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { signOut }
+from "next-auth/react";
+
+export default function
+AuthButtons() {
+
+  return (
+
+    <button
+
+      onClick={()=>
+        signOut()
+      }
+
+      className="
+      border
+      px-4
+      py-2
+      rounded"
+    >
+
+      Logout
+
+    </button>
+  );
+}
