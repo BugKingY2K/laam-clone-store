fffbe66f0c2a81704abd726ff0cb8ed21d852abd
BugKingY2K <waqsak@hotmail.com>
2026-06-18 22:42:02 +0500

    Update seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -36,6 +36,42 @@ async function main() {
   console.log("Admin Created");
 }
 
+const men =
+await prisma.category.create({
+
+  data: {
+
+    name:"Men",
+
+    slug:"men"
+  }
+});
+
+await prisma.product.createMany({
+
+  data:[
+
+    {
+      name:"Premium Polo Shirt",
+      slug:"premium-polo-shirt",
+      description:"Premium Cotton",
+      price:4500,
+      inventory:50,
+      categoryId:men.id
+    },
+
+    {
+      name:"Classic Oxford Shirt",
+      slug:"classic-oxford-shirt",
+      description:"Formal Wear",
+      price:6000,
+      inventory:40,
+      categoryId:men.id
+    }
+  ]
+});
+
+
 main()
   .catch(console.error)
   .finally(async () => {
