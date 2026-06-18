1c8fa6ab4ac6ec7f3d16a03fa5155feef215f073
BugKingY2K <waqsak@hotmail.com>
2026-06-18 21:05:53 +0500

    Create DashboardCard.tsx

---
+++ src/components/admin/DashboardCard.tsx
@@ -0,0 +1,36 @@
+type Props = {
+
+  title: string;
+
+  value: string;
+};
+
+export default function DashboardCard({
+  title,
+  value
+}: Props) {
+
+  return (
+
+    <div
+      className="
+      border
+      rounded-xl
+      p-6"
+    >
+
+      <h3 className="text-sm text-gray-500">
+
+        {title}
+
+      </h3>
+
+      <p className="text-3xl font-bold mt-2">
+
+        {value}
+
+      </p>
+
+    </div>
+  );
+}
