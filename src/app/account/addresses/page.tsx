54837360a6de768c30624fa18e31b1cd5c6ca46f
BugKingY2K <waqsak@hotmail.com>
2026-06-18 18:24:28 +0500

    Create page.tsx

---
+++ src/app/account/addresses/page.tsx
@@ -0,0 +1,15 @@
+export default function AddressPage() {
+
+  return (
+
+    <div className="container-custom py-10">
+
+      <h1 className="text-3xl font-bold">
+
+        Saved Addresses
+
+      </h1>
+
+    </div>
+  );
+}
