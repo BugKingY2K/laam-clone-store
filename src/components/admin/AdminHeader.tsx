b10a350c7b946e3104ce6916cd3c7086210ebe68
BugKingY2K <waqsak@hotmail.com>
2026-06-18 21:04:10 +0500

    Create AdminHeader.tsx

---
+++ src/components/admin/AdminHeader.tsx
@@ -0,0 +1,22 @@
+export default function AdminHeader() {
+
+  return (
+
+    <header
+      className="
+      border-b
+      h-16
+      flex
+      items-center
+      px-6"
+    >
+
+      <h1 className="font-semibold">
+
+        Admin Dashboard
+
+      </h1>
+
+    </header>
+  );
+}
