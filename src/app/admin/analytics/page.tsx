4a6b364ad77f749fec398dfe197460bdaaa49688
BugKingY2K <waqsak@hotmail.com>
2026-06-18 22:04:03 +0500

    Update page.tsx

--- src/app/admin/analytics/page.tsx
+++ src/app/admin/analytics/page.tsx
@@ -1,18 +1,60 @@
+"use client";
+
+import {
+
+  BarChart,
+
+  Bar,
+
+  XAxis,
+
+  YAxis
+
+} from "recharts";
+
+const data = [
+
+  {
+    month:"Jan",
+    sales:12000
+  },
+
+  {
+    month:"Feb",
+    sales:18000
+  },
+
+  {
+    month:"Mar",
+    sales:25000
+  }
+];
+
 export default function AnalyticsPage() {
 
   return (
 
     <div>
 
-      <h1
-        className="
-        text-3xl
-        font-bold"
-      >
+      <h1 className="text-3xl font-bold mb-6">
 
         Analytics
 
       </h1>
+
+      <BarChart
+        width={700}
+        height={300}
+        data={data}
+      >
+
+        <XAxis dataKey="month"/>
+
+        <YAxis/>
+
+        <Bar dataKey="sales"/>
+
+      </BarChart>
 
     </div>
   );
