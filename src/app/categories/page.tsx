5f3a108b175434c5736ad7fb5ab96243373748f1
BugKingY2K <waqsak@hotmail.com>
2026-06-18 17:58:00 +0500

    Create page.tsx

---
+++ src/app/categories/page.tsx
@@ -0,0 +1,16 @@
+export default function CategoriesPage() {
+
+  return (
+
+    <div className="container-custom py-12">
+
+      <h1 className="text-4xl font-bold">
+
+        Categories
+
+      </h1>
+
+    </div>
+
+  );
+}
