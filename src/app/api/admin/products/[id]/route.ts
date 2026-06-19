a870dd007cc2b2b587cc6262fe4eb328f6393f2f
BugKingY2K <waqsak@hotmail.com>
2026-06-19 17:47:51 +0500

    Update route.ts

--- src/app/api/admin/products/[id]/route.ts
+++ src/app/api/admin/products/[id]/route.ts
@@ -1,22 +1,47 @@
-import { prisma } from "@/lib/prisma";
+import { prisma }
+from "@/lib/prisma";
+
+import { requireAdmin }
+from "@/lib/auth";
+
+export async function PUT(
+  request: Request,
+  { params }: any
+) {
+
+  await requireAdmin();
+
+  const body =
+    await request.json();
+
+  const product =
+    await prisma.product.update({
+
+      where:{
+        id:params.id
+      },
+
+      data:body
+    });
+
+  return Response.json(product);
+}
 
 export async function DELETE(
   request: Request,
-  context: {
-    params: {
-      id: string;
-    };
-  }
+  { params }: any
 ) {
+
+  await requireAdmin();
 
   await prisma.product.delete({
 
-    where: {
-      id: context.params.id
+    where:{
+      id:params.id
     }
   });
 
   return Response.json({
-    success: true
+    success:true
   });
 }
