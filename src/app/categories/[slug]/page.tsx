994166edf40ebed97cdec44325e57ae805277d54
BugKingY2K <waqsak@hotmail.com>
2026-06-19 17:12:48 +0500

    Create page.tsx

---
+++ src/app/categories/[slug]/page.tsx
@@ -0,0 +1,56 @@
+import { prisma } from "@/lib/prisma";
+
+import ProductGrid
+from "@/components/storefront/ProductGrid";
+
+type Props = {
+
+  params: {
+
+    slug: string;
+  };
+};
+
+export default async function CategoryPage({
+  params
+}: Props) {
+
+  const category =
+    await prisma.category.findUnique({
+
+      where: {
+        slug: params.slug
+      },
+
+      include: {
+        products: true
+      }
+    });
+
+  if (!category) {
+
+    return <div>Category Not Found</div>;
+  }
+
+  return (
+
+    <div className="container-custom py-10">
+
+      <h1
+        className="
+        text-4xl
+        font-bold
+        mb-8"
+      >
+
+        {category.name}
+
+      </h1>
+
+      <ProductGrid
+        products={category.products}
+      />
+
+    </div>
+  );
+}
