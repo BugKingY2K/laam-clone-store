2363f46b245323c5678f243f0562b5442cd43050
BugKingY2K <waqsak@hotmail.com>
2026-06-19 17:12:07 +0500

    Update page.tsx

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,16 +1,63 @@
-export default function CategoriesPage() {
+import { prisma } from "@/lib/prisma";
+
+import Link from "next/link";
+
+export default async function CategoriesPage() {
+
+  const categories =
+    await prisma.category.findMany();
 
   return (
 
-    <div className="container-custom py-12">
+    <div className="container-custom py-10">
 
-      <h1 className="text-4xl font-bold">
+      <h1
+        className="
+        text-4xl
+        font-bold
+        mb-8"
+      >
 
         Categories
 
       </h1>
 
-    </div>
+      <div
+        className="
+        grid
+        md:grid-cols-4
+        gap-6"
+      >
 
+        {categories.map(category => (
+
+          <Link
+
+            key={category.id}
+
+            href={
+              `/categories/${category.slug}`
+            }
+
+          >
+
+            <div
+              className="
+              border
+              p-6
+              rounded-xl"
+            >
+
+              {category.name}
+
+            </div>
+
+          </Link>
+
+        ))}
+
+      </div>
+
+    </div>
   );
 }
