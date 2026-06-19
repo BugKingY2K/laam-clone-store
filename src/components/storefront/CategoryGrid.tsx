fd0887b4fe789b218d51d6ede62f4137428df7b2
BugKingY2K <waqsak@hotmail.com>
2026-06-19 17:20:24 +0500

    Update CategoryGrid.tsx

--- src/components/storefront/CategoryGrid.tsx
+++ src/components/storefront/CategoryGrid.tsx
@@ -1,69 +1,63 @@
-const categories = [
+import Link from "next/link";
 
-  {
-    name: "Men",
-    image:
-      "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f"
-  },
+export default function CategoryGrid({
 
-  {
-    name: "Women",
-    image:
-      "https://images.unsplash.com/photo-1496747611176-843222e1e57c"
-  },
+  categories
 
-  {
-    name: "Accessories",
-    image:
-      "https://images.unsplash.com/photo-1523170335258-f5ed11844a49"
-  },
-
-  {
-    name: "Footwear",
-    image:
-      "https://images.unsplash.com/photo-1542291026-7eec264c27ff"
-  }
-
-];
-
-export default function CategoryGrid() {
+}: {
+  categories:any[];
+}) {
 
   return (
 
-    <section className="container-custom py-16">
+    <section
+      className="
+      container-custom
+      py-16"
+    >
 
-      <h2 className="text-3xl font-bold mb-8">
+      <h2
+        className="
+        text-3xl
+        font-bold
+        mb-8"
+      >
 
-        Shop By Category
+        Categories
 
       </h2>
 
-      <div className="grid md:grid-cols-4 gap-6">
+      <div
+        className="
+        grid
+        md:grid-cols-4
+        gap-6"
+      >
 
         {categories.map(category => (
 
-          <div
-            key={category.name}
-            className="rounded-xl overflow-hidden"
+          <Link
+
+            key={category.id}
+
+            href={
+              `/categories/${category.slug}`
+            }
+
           >
 
-            <img
-              src={category.image}
-              alt={category.name}
-              className="h-72 w-full object-cover"
-            />
+            <div
+              className="
+              border
+              rounded-xl
+              p-6"
+            >
 
-            <div className="p-4">
-
-              <h3 className="font-semibold">
-
-                {category.name}
-
-              </h3>
+              {category.name}
 
             </div>
 
-          </div>
+          </Link>
 
         ))}
 
