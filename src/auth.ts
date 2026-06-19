0f91f71f4acacd3fbad48450eaed16f02e8007cf
BugKingY2K <waqsak@hotmail.com>
2026-06-19 18:54:17 +0500

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -66,4 +66,25 @@ export const {
 
     signIn: "/login"
   }
+  callbacks: {
+
+  async jwt({ token, user }) {
+
+    if (user) {
+      token.role = (user as any).role;
+    }
+
+    return token;
+  },
+
+  async session({ session, token }) {
+
+    if (session.user) {
+      (session.user as any).role =
+        token.role;
+    }
+
+    return session;
+  }
+}
 });
