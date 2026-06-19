d86eeb8897340f95404818ba0effb073c4e1f307
BugKingY2K <waqsak@hotmail.com>
2026-06-19 17:42:27 +0500

    Create route.ts

---
+++ src/app/api/cart/item/route.ts
@@ -0,0 +1,28 @@
+import { prisma }
+from "@/lib/prisma";
+
+export async function POST(
+  request: Request
+) {
+
+  const body =
+    await request.json();
+
+  const item =
+    await prisma.cartItem.create({
+
+      data:{
+
+        cartId:
+          body.cartId,
+
+        productId:
+          body.productId,
+
+        quantity:
+          body.quantity
+      }
+    });
+
+  return Response.json(item);
+}
