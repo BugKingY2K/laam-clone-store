7b014a42b6615f397f51da5365c77e01e3890b0e
BugKingY2K <waqsak@hotmail.com>
2026-06-20 15:17:56 +0500

    Update page.tsx

--- src/app/account/order/page.tsx
+++ src/app/account/order/page.tsx
@@ -1,8 +1,6 @@
-import { auth }
-from "@/auth";
+import { auth } from "@/auth";
 
-import { prisma }
-from "@/lib/prisma";
+import { prisma } from "@/lib/prisma";
 
 export default async function OrdersPage() {
 
