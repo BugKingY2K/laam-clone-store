028d1bfe46bcefc4ebb35b5c5f4a3643dde4081d
BugKingY2K <waqsak@hotmail.com>
2026-06-20 14:45:32 +0500

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,37 +1,25 @@
 import "next-auth";
 
 declare module "next-auth" {
-
   interface Session {
-
     user: {
-
       id: string;
-
       role: string;
-
       name?: string | null;
-
       email?: string | null;
-
       image?: string | null;
     };
   }
 
   interface User {
-
     id: string;
-
     role: string;
   }
 }
 
 declare module "@auth/core/jwt" {
-
   interface JWT {
-
     id: string;
-
     role: string;
   }
 }
