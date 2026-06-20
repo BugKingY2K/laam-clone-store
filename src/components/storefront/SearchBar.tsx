a89bda34facb228303b8b48afd8c447f40d6032b
BugKingY2K <waqsak@hotmail.com>
2026-06-20 15:31:10 +0500

    Update SearchBar.tsx

--- src/components/storefront/SearchBar.tsx
+++ src/components/storefront/SearchBar.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useRouter }
-from "next/navigation";
+import { useRouter } from "next/navigation";
 
-import { useState }
-from "react";
+import { useState } from "react";
 
 export default function SearchBar() {
 
